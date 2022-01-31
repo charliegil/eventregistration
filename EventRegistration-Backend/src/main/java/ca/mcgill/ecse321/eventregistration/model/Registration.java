21aca499a53863202bfe61fb6dc8e4d375ad3c19
Charlie Gil <charliegil0691@gmail.com>
2022-01-31 16:33:26 -0500

    Database setup and testing

--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/model/Registration.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/model/Registration.java
@@ -4,149 +4,37 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
-//%% NEW FILE Registration BEGINS HERE %%
-
-/*PLEASE DO NOT EDIT THIS CODE*/
-/*This code was generated using the UMPLE 1.31.1.5860.78bb27cc6 modeling language!*/
-
-
-
-// line 19 "model.ump"
-// line 63 "model.ump"
 @Entity
-public class Registration
-{
-
-  //------------------------
-  // MEMBER VARIABLES
-  //------------------------
-
-  //Registration Attributes
-  private int id;
-
-  //Registration Associations
-  private RegistrationManager registrationManager;
-  private Event event;
-  private Person person;
-
-  //------------------------
-  // CONSTRUCTOR
-  //------------------------
-
-  public Registration(int aId, RegistrationManager aRegistrationManager, Event aEvent, Person aPerson)
-  {
-    id = aId;
-    boolean didAddRegistrationManager = setRegistrationManager(aRegistrationManager);
-    if (!didAddRegistrationManager)
-    {
-      throw new RuntimeException("Unable to create registration due to registrationManager. See http://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
+public class Registration{
+private int id;
+   
+   public void setId(int value) {
+this.id = value;
     }
-    if (!setEvent(aEvent))
-    {
-      throw new RuntimeException("Unable to create Registration due to aEvent. See http://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
+@Id
+public int getId() {
+return this.id;
     }
-    if (!setPerson(aPerson))
-    {
-      throw new RuntimeException("Unable to create Registration due to aPerson. See http://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
-    }
-  }
+private Person person;
 
-  //------------------------
-  // INTERFACE
-  //------------------------
+@ManyToOne(optional=false)
+public Person getPerson() {
+   return this.person;
+}
 
-  public boolean setId(int aId)
-  {
-    boolean wasSet = false;
-    id = aId;
-    wasSet = true;
-    return wasSet;
-  }
+public void setPerson(Person person) {
+   this.person = person;
+}
 
-  @Id
-  public int getId()
-  {
-    return id;
-  }
-  /* Code from template association_GetOne */
-  @ManyToOne(optional = false)
-  public RegistrationManager getRegistrationManager()
-  {
-    return registrationManager;
-  }
-  /* Code from template association_GetOne */
-  @ManyToOne(optional = false)
-  public Event getEvent()
-  {
-    return event;
-  }
-  /* Code from template association_GetOne */
-  @ManyToOne(optional = false)
-  public Person getPerson()
-  {
-    return person;
-  }
-  /* Code from template association_SetOneToMany */
-  public boolean setRegistrationManager(RegistrationManager aRegistrationManager)
-  {
-    boolean wasSet = false;
-    if (aRegistrationManager == null)
-    {
-      return wasSet;
-    }
+private Event event;
 
-    RegistrationManager existingRegistrationManager = registrationManager;
-    registrationManager = aRegistrationManager;
-    if (existingRegistrationManager != null && !existingRegistrationManager.equals(aRegistrationManager))
-    {
-      existingRegistrationManager.removeRegistration(this);
-    }
-    registrationManager.addRegistration(this);
-    wasSet = true;
-    return wasSet;
-  }
-  /* Code from template association_SetUnidirectionalOne */
-  public boolean setEvent(Event aNewEvent)
-  {
-    boolean wasSet = false;
-    if (aNewEvent != null)
-    {
-      event = aNewEvent;
-      wasSet = true;
-    }
-    return wasSet;
-  }
-  /* Code from template association_SetUnidirectionalOne */
-  public boolean setPerson(Person aNewPerson)
-  {
-    boolean wasSet = false;
-    if (aNewPerson != null)
-    {
-      person = aNewPerson;
-      wasSet = true;
-    }
-    return wasSet;
-  }
+@ManyToOne(optional=false)
+public Event getEvent() {
+   return this.event;
+}
 
-  public void delete()
-  {
-    RegistrationManager placeholderRegistrationManager = registrationManager;
-    this.registrationManager = null;
-    if(placeholderRegistrationManager != null)
-    {
-      placeholderRegistrationManager.removeRegistration(this);
-    }
-    event = null;
-    person = null;
-  }
+public void setEvent(Event event) {
+   this.event = event;
+}
 
-
-  public String toString()
-  {
-    return super.toString() + "["+
-            "id" + ":" + getId()+ "]" + System.getProperties().getProperty("line.separator") +
-            "  " + "registrationManager = "+(getRegistrationManager()!=null?Integer.toHexString(System.identityHashCode(getRegistrationManager())):"null") + System.getProperties().getProperty("line.separator") +
-            "  " + "event = "+(getEvent()!=null?Integer.toHexString(System.identityHashCode(getEvent())):"null") + System.getProperties().getProperty("line.separator") +
-            "  " + "person = "+(getPerson()!=null?Integer.toHexString(System.identityHashCode(getPerson())):"null");
-  }
 }
--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/model/RegistrationManager.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/model/RegistrationManager.java
@@ -1,382 +1,53 @@
 package ca.mcgill.ecse321.eventregistration.model;
 
-//%% NEW FILE RegistrationManager BEGINS HERE %%
-
-/*PLEASE DO NOT EDIT THIS CODE*/
-/*This code was generated using the UMPLE 1.31.1.5860.78bb27cc6 modeling language!*/
-
-
-import java.util.*;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import java.util.Set;
+import javax.persistence.CascadeType;
 import javax.persistence.OneToMany;
+import javax.persistence.Id;
 
-import java.sql.Date;
-import java.sql.Time;
-
-// line 2 "model.ump"
-// line 48 "model.ump"
 @Entity
-public class RegistrationManager
-{
+public class RegistrationManager{
+private Set<Person> persons;
 
-  //------------------------
-  // MEMBER VARIABLES
-  //------------------------
-
-  //RegistrationManager Associations
-  private List<Registration> registrations;
-  private List<Event> events;
-  private List<Person> persons;
-
-  //------------------------
-  // CONSTRUCTOR
-  //------------------------
-
-  public RegistrationManager()
-  {
-    registrations = new ArrayList<Registration>();
-    events = new ArrayList<Event>();
-    persons = new ArrayList<Person>();
-  }
-
-  //------------------------
-  // INTERFACE
-  //------------------------
-  /* Code from template association_GetMany */
-  @OneToMany(cascade={CascadeType.ALL})
-  public Registration getRegistration(int index)
-  {
-    Registration aRegistration = registrations.get(index);
-    return aRegistration;
-  }
-
-  public List<Registration> getRegistrations()
-  {
-    List<Registration> newRegistrations = Collections.unmodifiableList(registrations);
-    return newRegistrations;
-  }
-
-  public int numberOfRegistrations()
-  {
-    int number = registrations.size();
-    return number;
-  }
-
-  public boolean hasRegistrations()
-  {
-    boolean has = registrations.size() > 0;
-    return has;
-  }
-
-  public int indexOfRegistration(Registration aRegistration)
-  {
-    int index = registrations.indexOf(aRegistration);
-    return index;
-  }
-  /* Code from template association_GetMany */
-  @OneToMany(cascade = {CascadeType.ALL})
-  public Event getEvent(int index)
-  {
-    Event aEvent = events.get(index);
-    return aEvent;
-  }
-
-  public List<Event> getEvents()
-  {
-    List<Event> newEvents = Collections.unmodifiableList(events);
-    return newEvents;
-  }
-
-  public int numberOfEvents()
-  {
-    int number = events.size();
-    return number;
-  }
-
-  public boolean hasEvents()
-  {
-    boolean has = events.size() > 0;
-    return has;
-  }
-
-  public int indexOfEvent(Event aEvent)
-  {
-    int index = events.indexOf(aEvent);
-    return index;
-  }
-  /* Code from template association_GetMany */
-  @OneToMany(cascade = {CascadeType.ALL})
-  public Person getPerson(int index)
-  {
-    Person aPerson = persons.get(index);
-    return aPerson;
-  }
-
-  public List<Person> getPersons()
-  {
-    List<Person> newPersons = Collections.unmodifiableList(persons);
-    return newPersons;
-  }
-
-  public int numberOfPersons()
-  {
-    int number = persons.size();
-    return number;
-  }
-
-  public boolean hasPersons()
-  {
-    boolean has = persons.size() > 0;
-    return has;
-  }
-
-  public int indexOfPerson(Person aPerson)
-  {
-    int index = persons.indexOf(aPerson);
-    return index;
-  }
-  /* Code from template association_MinimumNumberOfMethod */
-  public static int minimumNumberOfRegistrations()
-  {
-    return 0;
-  }
-  /* Code from template association_AddManyToOne */
-  public Registration addRegistration(int aId, Event aEvent, Person aPerson)
-  {
-    return new Registration(aId, this, aEvent, aPerson);
-  }
-
-  public boolean addRegistration(Registration aRegistration)
-  {
-    boolean wasAdded = false;
-    if (registrations.contains(aRegistration)) { return false; }
-    RegistrationManager existingRegistrationManager = aRegistration.getRegistrationManager();
-    boolean isNewRegistrationManager = existingRegistrationManager != null && !this.equals(existingRegistrationManager);
-    if (isNewRegistrationManager)
-    {
-      aRegistration.setRegistrationManager(this);
-    }
-    else
-    {
-      registrations.add(aRegistration);
-    }
-    wasAdded = true;
-    return wasAdded;
-  }
-
-  public boolean removeRegistration(Registration aRegistration)
-  {
-    boolean wasRemoved = false;
-    //Unable to remove aRegistration, as it must always have a registrationManager
-    if (!this.equals(aRegistration.getRegistrationManager()))
-    {
-      registrations.remove(aRegistration);
-      wasRemoved = true;
-    }
-    return wasRemoved;
-  }
-  /* Code from template association_AddIndexControlFunctions */
-  public boolean addRegistrationAt(Registration aRegistration, int index)
-  {  
-    boolean wasAdded = false;
-    if(addRegistration(aRegistration))
-    {
-      if(index < 0 ) { index = 0; }
-      if(index > numberOfRegistrations()) { index = numberOfRegistrations() - 1; }
-      registrations.remove(aRegistration);
-      registrations.add(index, aRegistration);
-      wasAdded = true;
-    }
-    return wasAdded;
-  }
-
-  public boolean addOrMoveRegistrationAt(Registration aRegistration, int index)
-  {
-    boolean wasAdded = false;
-    if(registrations.contains(aRegistration))
-    {
-      if(index < 0 ) { index = 0; }
-      if(index > numberOfRegistrations()) { index = numberOfRegistrations() - 1; }
-      registrations.remove(aRegistration);
-      registrations.add(index, aRegistration);
-      wasAdded = true;
-    } 
-    else 
-    {
-      wasAdded = addRegistrationAt(aRegistration, index);
-    }
-    return wasAdded;
-  }
-  /* Code from template association_MinimumNumberOfMethod */
-  public static int minimumNumberOfEvents()
-  {
-    return 0;
-  }
-  /* Code from template association_AddManyToOne */
-  public Event addEvent(String aName, Date aEventDate, Time aStartTime, Time aEndTime)
-  {
-    return new Event(aName, aEventDate, aStartTime, aEndTime, this);
-  }
-
-  public boolean addEvent(Event aEvent)
-  {
-    boolean wasAdded = false;
-    if (events.contains(aEvent)) { return false; }
-    RegistrationManager existingRegistrationManager = aEvent.getRegistrationManager();
-    boolean isNewRegistrationManager = existingRegistrationManager != null && !this.equals(existingRegistrationManager);
-    if (isNewRegistrationManager)
-    {
-      aEvent.setRegistrationManager(this);
-    }
-    else
-    {
-      events.add(aEvent);
-    }
-    wasAdded = true;
-    return wasAdded;
-  }
-
-  public boolean removeEvent(Event aEvent)
-  {
-    boolean wasRemoved = false;
-    //Unable to remove aEvent, as it must always have a registrationManager
-    if (!this.equals(aEvent.getRegistrationManager()))
-    {
-      events.remove(aEvent);
-      wasRemoved = true;
-    }
-    return wasRemoved;
-  }
-  /* Code from template association_AddIndexControlFunctions */
-  public boolean addEventAt(Event aEvent, int index)
-  {  
-    boolean wasAdded = false;
-    if(addEvent(aEvent))
-    {
-      if(index < 0 ) { index = 0; }
-      if(index > numberOfEvents()) { index = numberOfEvents() - 1; }
-      events.remove(aEvent);
-      events.add(index, aEvent);
-      wasAdded = true;
-    }
-    return wasAdded;
-  }
-
-  public boolean addOrMoveEventAt(Event aEvent, int index)
-  {
-    boolean wasAdded = false;
-    if(events.contains(aEvent))
-    {
-      if(index < 0 ) { index = 0; }
-      if(index > numberOfEvents()) { index = numberOfEvents() - 1; }
-      events.remove(aEvent);
-      events.add(index, aEvent);
-      wasAdded = true;
-    } 
-    else 
-    {
-      wasAdded = addEventAt(aEvent, index);
-    }
-    return wasAdded;
-  }
-  /* Code from template association_MinimumNumberOfMethod */
-  public static int minimumNumberOfPersons()
-  {
-    return 0;
-  }
-  /* Code from template association_AddManyToOne */
-  public Person addPerson(String aName)
-  {
-    return new Person(aName, this);
-  }
-
-  public boolean addPerson(Person aPerson)
-  {
-    boolean wasAdded = false;
-    if (persons.contains(aPerson)) { return false; }
-    RegistrationManager existingRegistrationManager = aPerson.getRegistrationManager();
-    boolean isNewRegistrationManager = existingRegistrationManager != null && !this.equals(existingRegistrationManager);
-    if (isNewRegistrationManager)
-    {
-      aPerson.setRegistrationManager(this);
-    }
-    else
-    {
-      persons.add(aPerson);
-    }
-    wasAdded = true;
-    return wasAdded;
-  }
-
-  public boolean removePerson(Person aPerson)
-  {
-    boolean wasRemoved = false;
-    //Unable to remove aPerson, as it must always have a registrationManager
-    if (!this.equals(aPerson.getRegistrationManager()))
-    {
-      persons.remove(aPerson);
-      wasRemoved = true;
-    }
-    return wasRemoved;
-  }
-  /* Code from template association_AddIndexControlFunctions */
-  public boolean addPersonAt(Person aPerson, int index)
-  {  
-    boolean wasAdded = false;
-    if(addPerson(aPerson))
-    {
-      if(index < 0 ) { index = 0; }
-      if(index > numberOfPersons()) { index = numberOfPersons() - 1; }
-      persons.remove(aPerson);
-      persons.add(index, aPerson);
-      wasAdded = true;
-    }
-    return wasAdded;
-  }
-
-  public boolean addOrMovePersonAt(Person aPerson, int index)
-  {
-    boolean wasAdded = false;
-    if(persons.contains(aPerson))
-    {
-      if(index < 0 ) { index = 0; }
-      if(index > numberOfPersons()) { index = numberOfPersons() - 1; }
-      persons.remove(aPerson);
-      persons.add(index, aPerson);
-      wasAdded = true;
-    } 
-    else 
-    {
-      wasAdded = addPersonAt(aPerson, index);
-    }
-    return wasAdded;
-  }
-
-  public void delete()
-  {
-    while (registrations.size() > 0)
-    {
-      Registration aRegistration = registrations.get(registrations.size() - 1);
-      aRegistration.delete();
-      registrations.remove(aRegistration);
-    }
-    
-    while (events.size() > 0)
-    {
-      Event aEvent = events.get(events.size() - 1);
-      aEvent.delete();
-      events.remove(aEvent);
-    }
-    
-    while (persons.size() > 0)
-    {
-      Person aPerson = persons.get(persons.size() - 1);
-      aPerson.delete();
-      persons.remove(aPerson);
-    }
-    
-  }
-
+@OneToMany(cascade={CascadeType.ALL})
+public Set<Person> getPersons() {
+   return this.persons;
 }
+
+public void setPersons(Set<Person> persons) {
+   this.persons = persons;
+}
+
+private Set<Registration> registrations;
+
+@OneToMany(cascade={CascadeType.ALL})
+public Set<Registration> getRegistrations() {
+   return this.registrations;
+}
+
+public void setRegistrations(Set<Registration> registrations) {
+   this.registrations = registrations;
+}
+
+private Set<Event> events;
+
+@OneToMany(cascade={CascadeType.ALL})
+public Set<Event> getEvents() {
+   return this.events;
+}
+
+public void setEvents(Set<Event> events) {
+   this.events = events;
+}
+
+private int id;
+
+public void setId(int value) {
+this.id = value;
+    }
+@Id
+public int getId() {
+return this.id;
+       }
+   }
